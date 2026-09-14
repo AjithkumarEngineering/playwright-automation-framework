2a0efe651e5b474f3a04afc74c5898eac0f8fa34
AjithkumarEngineering <ajithkumarengineering@gmail.com>
2026-09-14 12:16:09 +0530

    Add Codegen test

---
+++ tests/Codegen.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+//below code used tp written by automatic codegen playwright
+
+test('test', async ({ page }) => {
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+  await page.getByRole('textbox', { name: 'Username:' }).click();
+  await page.getByRole('textbox', { name: 'Username:' }).fill('rahulshettyacademy');
+  await page.getByRole('textbox', { name: 'Password:' }).click();
+  await page.getByRole('textbox', { name: 'Password:' }).fill('Learning@830$3mK2');
+  await page.locator('span').nth(4).click();
+  await page.getByRole('button', { name: 'Okay' }).click();
+  await page.getByRole('combobox').selectOption('consult');
+  await page.getByRole('checkbox', { name: 'I Agree to the terms and' }).check();
+  await expect(page.getByRole('button', { name: 'Sign In' })).toBeVisible();
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.waitForLoadState();
+  await expect(page.locator('app-card-list')).toContainText('iphone X');
+});
